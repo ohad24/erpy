b34a6c9d86d4b29ec626821055f1f79c1c983f33
Ohad Mizrachi <ohad24@gmail.com>
2019-12-28 21:05:49 +0200

    update sql infra

--- src/sql/001_DB_Infrastructure.sql
+++ src/sql/001_DB_Infrastructure.sql
@@ -6,8 +6,8 @@ CREATE USER erpy WITH
     INHERIT
     NOREPLICATION
     CONNECTION LIMIT -1
-    PASSWORD :postgres_password; --FIXME: set password variable
-GRANT postgres TO erpy WITH ADMIN OPTION; --FIXME: remove this (or alter)
+    PASSWORD :postgres_password;
+
 -- check DB TZ on dba
 ALTER USER erpy SET timezone=:tz;
 
@@ -20,16 +20,9 @@ CREATE DATABASE :postgres_db
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1
     LC_COLLATE = 'he_IL.utf8'
-	  LC_CTYPE = 'he_IL.utf8'
-	  TEMPLATE 'template0';
+	LC_CTYPE = 'he_IL.utf8'
+	TEMPLATE 'template0';
 
 ALTER DATABASE :postgres_db SET lc_time TO :lang;
 
--- GRANT CONNECT ON DATABASE :env TO :app_user;
--- SET lc_time TO 'he_IL.utf8';
-
-
--- CREATE USER :app_user WITH PASSWORD 'py_app321!';
--- ALTER USER :app_user SET timezone='Asia/Jerusalem';
-
-
+GRANT ALL PRIVILEGES ON DATABASE :postgres_db TO erpy;
